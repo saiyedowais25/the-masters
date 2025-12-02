2830a9df1d7e69f24116f1e70d65063f89e8f788
saiyedowais25 <owaisdepapel@gmail.com>
2025-12-02 17:17:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import RootNavigator from './navigation/RootNavigator';
+export default function App(){ return <RootNavigator/>; }
---
+++ theme.js
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    background: '#060606', // deep black
+    card: '#0f0f0f',
+    text: '#f7f5f0',
+    gold: '#bfa34a', // matte gold
+    muted: '#9d947f'
+  },
+  spacing: (n) => n * 8
+};
